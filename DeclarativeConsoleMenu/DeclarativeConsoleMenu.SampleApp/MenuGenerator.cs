911f9987d702319a898c2aac95e2f77c273d7296
Alex Young <alex@alexyoung.info>
2016-12-15 19:12:04 +0000

    remove usings

--- DeclarativeConsoleMenu/DeclarativeConsoleMenu.SampleApp/MenuGenerator.cs
+++ DeclarativeConsoleMenu/DeclarativeConsoleMenu.SampleApp/MenuGenerator.cs
@@ -1,9 +1,4 @@
-﻿using DeclarativeConsoleMenu;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace DeclarativeConsoleMenu.SampleApp
 {
